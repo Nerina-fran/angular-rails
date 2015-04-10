6bb29d8c07eaa858212cb078b724d3f1d1bd5e21
Trevoir Williams <trevoir.williams@gmail.com>
2015-04-10 13:11:50 +0000

    Initial Commit. Project Skeleton without User Authentication

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,30 @@
+// angular.module('flapperNews', ['ui.router', 'templates'])
+// .controller('MainCtrl', [
+// '$scope',
+// function($scope){
+//   $scope.test = 'Hello world!';
+// }]);
+
+angular.module('flapperNews', ['ui.router'])
+.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: 'home/_home.html',
+      controller: 'MainCtrl'
+    })
+    .state('posts', {
+      url: '/posts/{id}',
+      templateUrl: 'posts/_posts.html',
+      controller: 'PostsCtrl'
+    });
+
+  $urlRouterProvider.otherwise('home');
+}]);
+
+
+
+
