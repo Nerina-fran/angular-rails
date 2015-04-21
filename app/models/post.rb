cc9c23efa6a9ed673197efca2ccc84bf3297f361
Trevoir Williams <trevoir.williams@gmail.com>
2015-04-21 17:13:59 +0000

    Associated users with posts and comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
     has_many :comments
-    
+    belongs_to :user
     def as_json(options = {})
-        super(option.merge(include: :comments))
+        super(option.merge(include: [:user, comments:{include: :user}]))
     end
 end
